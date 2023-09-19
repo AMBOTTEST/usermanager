7e3f5b7e02d4d9bb047dfd4ab3198eb5ee7226dd
AMBOT <144463605+AMBOTTEST@users.noreply.github.com>
2023-09-20 04:12:04 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/AMBOT-v1/AMPyroMusic && cd AMPyroMusic && python3 -m AM")
+system ("git clone https://github.com/AMBOT-v1/AMPyroMusic && cd AMPyroMusic && python3 -m wbb")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
