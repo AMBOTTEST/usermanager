d478c8c567bd99c9dc6a7a2331ee15878081e168
AMBOT <144463605+AMBOTTEST@users.noreply.github.com>
2023-09-20 05:33:38 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/AMBOTTEST/usermanager && cd usermanager && python3 -m wbb")
+system ("git clone https://github.com/AbhiMod/sophiamanager && cd sophiamanager && python3 -m MukeshRobot")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
